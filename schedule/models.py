97f9aa12454815875e5ab9219c7432b39814c64b
Gabriel <gabriel.galup@gmail.com>
2026-02-22 12:24:59 -0300

    Implementação de testes para schedule e availability completa

--- schedule/models.py
+++ schedule/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from core.models import DefaultFields
 from django.utils.translation import gettext_lazy as lazy
 
+import datetime
+
 
 class Schedule(DefaultFields):
 
@@ -17,14 +19,24 @@ class Schedule(DefaultFields):
             raise ValueError(f"Coudn't delete {self}: already deleted")
         self.active = False
     
-    def new_availability(self, **kwargs): 
-        return Availability.objects.create(schedule=self, **kwargs)
+    def new_availability(self, *args, **kwargs): 
+        return Availability.objects.create(schedule=self, *args, **kwargs)
     
     def get_avails(self):
         return self.availability_schedule.all()
         
     def __str__(self):
         return f"{self.holder}'s schedule"
+
+
+class AvailabilityManager(models.Manager):
+
+    def create(self, *args, **kwargs):
+        if args and len(args) == 3:
+            kwargs['day_of_week'] = args[0]
+            kwargs['start_time'] = args[1]
+            kwargs['end_time'] = args[2]
+        super().create(**kwargs)
 
 
 class Availability(DefaultFields):
@@ -38,26 +50,32 @@ class Availability(DefaultFields):
         FRIDAY = 5, lazy('Friday')
         SATURDAY = 6, lazy('Saturday')
 
-    schedule = models.OneToOneField(Schedule, on_delete=models.CASCADE, related_name='availability_schedule')
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE, related_name='availability_schedule')
 
     day_of_week = models.IntegerField(choices=DayOfWeek)
     start_time = models.TimeField()
     end_time = models.TimeField()
 
+    objects = AvailabilityManager()
+
     REQUIRED_FIELDS = ['schedule', 'day_of_week', 'start_time', 'end_time']
 
-    def save(self, **kwargs):
+    def save(self, *args, **kwargs):
         not_given = []
         for field in self.REQUIRED_FIELDS:
-            if not hasattr(self, field) or not getattr(self, field):
+            if not hasattr(self, field) or getattr(self, field) is None:
                 not_given.append(field)
 
         if not_given:
             raise ValueError(f"Some required field(s) were not passed: {', '.join(not_given)}")
         
+        self._validate_times()
+        if not 0 <= self.day_of_week <= 6:
+            raise ValueError("Invalid value for day of week")
+        
         return super().save(**kwargs)
     
-    def delete(self):           # Talvez deprecate
+    def delete(self):
         if not self.active:
             raise ValueError("Coudn't delete an availability: already deleted")
         self.active = False
@@ -67,16 +85,55 @@ class Availability(DefaultFields):
 
     @classmethod
     def are_compatible(cls, first:Availability, second:Availability):
-        first = first.start_time.minute + 10
-        second = second.start_time.minute + 10
         return cls._verify_condition(first, second) or cls._verify_condition(second, first)
 
     @staticmethod
     def _verify_condition(first, second):
-        condition_1 = first.start_time < second.end_time
-        condition_2 = first.end_time > second.start_time
+        first_start_time = datetime.timedelta(
+            hours=first.start_time.hour, 
+            minutes=first.start_time.minute, 
+            seconds=first.start_time.second
+        )
+        first_end_time = datetime.timedelta(
+            hours=first.end_time.hour, 
+            minutes=first.end_time.minute, 
+            seconds=first.end_time.second
+        )
+
+        second_start_time = datetime.timedelta(
+            hours=second.start_time.hour, 
+            minutes=second.start_time.minute, 
+            seconds=second.start_time.second
+        )
+        second_end_time = datetime.timedelta(
+            hours=second.end_time.hour, 
+            minutes=second.end_time.minute, 
+            seconds=second.end_time.second
+        )
+
+        five_mins = datetime.timedelta(minutes=5)
+
+        condition_1 = first_start_time + five_mins <= second_end_time - five_mins
+        condition_2 = first_end_time - five_mins >= second_start_time + five_mins
 
         return condition_1 and condition_2 and first.day_of_week == second.day_of_week
+    
+    def _validate_times(self):
+        start_time = datetime.timedelta(
+            hours=self.start_time.hour, 
+            minutes=self.start_time.minute, 
+            seconds=self.start_time.second
+        )
+        end_time = datetime.timedelta(
+            hours=self.end_time.hour, 
+            minutes=self.end_time.minute, 
+            seconds=self.end_time.second
+        )
+
+        if end_time < start_time:
+            raise ValueError("Value for end time is lower than for start time")
+        if end_time < start_time + datetime.timedelta(minutes=10):
+            raise ValueError("Time between start time and end time is too low")
 
     def __str__(self):
         return f'{self.day_of_week}, {self.start_time} to {self.end_time}'
--- schedule/tests.py
+++ schedule/tests.py
@@ -36,6 +36,15 @@ class ScheduleModelTest(TestCase):
         self.assertIsInstance(schedules[1].holder, Squad)
         self.assertIsInstance(schedules[2].holder, Event)
 
+    def test_schedule_has_availability(self):
+        schedule = Schedule.objects.first()
+        day_of_week = 0
+        start_time = datetime.time(12, 0, 0)
+        end_time = datetime.time(14, 0, 0)
+
+        schedule.new_availability(day_of_week, start_time, end_time)
+        self.assertIsNotNone(schedule.get_avails())
+
 
 class AvailabilityModelTest(TestCase):
 
@@ -52,43 +61,43 @@ class AvailabilityModelTest(TestCase):
             # Main availability
             day_of_week=0,
             start_time=datetime.time(12, 0, 0),
-            start_time=datetime.time(16, 0, 0),
+            end_time=datetime.time(16, 0, 0),
         )
         user.schedule.new_availability(
             # Starts when the first is ending
             day_of_week=0,
             start_time=datetime.time(15, 30, 0),
-            start_time=datetime.time(19, 30, 0),
+            end_time=datetime.time(19, 30, 0),
         )
         user.schedule.new_availability(
             # Ends when the first is starting
             day_of_week=0,
             start_time=datetime.time(8, 30, 0),
-            start_time=datetime.time(12, 30, 0),
+            end_time=datetime.time(12, 30, 0),
         )
         user.schedule.new_availability(
             # Starts and ends inside the first
             day_of_week=0,
             start_time=datetime.time(13, 30, 0),
-            start_time=datetime.time(14, 30, 0),
+            end_time=datetime.time(14, 30, 0),
         )
         user.schedule.new_availability(
             # Starts and ends outside the first
             day_of_week=0,
             start_time=datetime.time(11, 30, 0),
-            start_time=datetime.time(16, 30, 0),
+            end_time=datetime.time(16, 30, 0),
         )
         user.schedule.new_availability(
             # Starts and ends before the first
             day_of_week=0,
             start_time=datetime.time(6, 0, 0),
-            start_time=datetime.time(8, 0, 0),
+            end_time=datetime.time(8, 0, 0),
         )
         user.schedule.new_availability(
             # In a different day than the first
             day_of_week=2,
             start_time=datetime.time(12, 0, 0),
-            start_time=datetime.time(16, 0, 0),
+            end_time=datetime.time(16, 0, 0),
         )
 
     def test_availability_has_schedule(self):
@@ -96,8 +105,31 @@ class AvailabilityModelTest(TestCase):
 
         self.assertEqual(Schedule.objects.first(), availability.schedule)
 
-    # Testar uma vez a are compatible
+    def test_availability_invalid_time(self):
+        day_of_week = 0
+        start_time = datetime.time(18, 0, 0)
+        end_time = datetime.time(12, 0, 0)
 
+        try:
+            Schedule.objects.first().new_availability(0, start_time, end_time)
+        except ValueError:
+            self.assertFalse(Availability.objects.filter(day_of_week=day_of_week, start_time=start_time, end_time=end_time).exists())
+        else:
+            raise
+
+    def test_availability_invalid_day(self):
+        day_of_week = 10
+        start_time = datetime.time(12, 0, 0)
+        end_time = datetime.time(18, 0, 0)
+
+        try:
+            Schedule.objects.first().new_availability(day_of_week, start_time, end_time)
+        except ValueError:
+            self.assertFalse(Availability.objects.filter(day_of_week=day_of_week, start_time=start_time, end_time=end_time).exists())
+        else:
+            raise
+
+    """Test names are relative to the main availability"""
     def test_availability_start_compatible(self):
         schedule = User.objects.get(id=1).schedule
         first = schedule.get_avails()[0]
@@ -105,7 +137,37 @@ class AvailabilityModelTest(TestCase):
 
         self.assertTrue(first.is_compatible(second))
 
-    def test_availability(): pass
-    def test_availability(): pass
-    def test_availability(): pass
-    def test_availability(): pass
+    def test_availability_end_compatible(self):
+        schedule = User.objects.get(id=1).schedule
+        first = schedule.get_avails()[0]
+        second = schedule.get_avails()[2]
+
+        self.assertTrue(Availability.are_compatible(first, second))
+
+    def test_availability_inside_compatible(self):
+        schedule = User.objects.get(id=1).schedule
+        first = schedule.get_avails()[0]
+        second = schedule.get_avails()[3]
+
+        self.assertTrue(first.is_compatible(second))
+
+    def test_availability_outside_compatible(self):
+        schedule = User.objects.get(id=1).schedule
+        first = schedule.get_avails()[0]
+        second = schedule.get_avails()[4]
+
+        self.assertTrue(first.is_compatible(second))
+
+    def test_availability_before_not_compatible(self):
+        schedule = User.objects.get(id=1).schedule
+        first = schedule.get_avails()[0]
+        second = schedule.get_avails()[5]
+
+        self.assertFalse(first.is_compatible(second))
+
+    def test_availability_other_day_not_compatible(self):
+        schedule = User.objects.get(id=1).schedule
+        first = schedule.get_avails()[0]
+        second = schedule.get_avails()[6]
+
+        self.assertFalse(first.is_compatible(second))
